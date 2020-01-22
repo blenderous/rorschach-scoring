75c519663cf661065d21dd2a50817362ea4e85d7
blenderous <hpone.n.only@gmail.com>
2020-01-22 21:45:49 +0530

    Updates

---
+++ precache-manifest.051c631479a81fc5ac7c2e32ede435d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f12ba7066f5030c81f52658d9580ec",
+    "url": "/rorschach-scoring/index.html"
+  },
+  {
+    "revision": "cde4594f9a279d87d9e8",
+    "url": "/rorschach-scoring/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "307c8922b23e2ed23247",
+    "url": "/rorschach-scoring/static/js/2.7ee57c45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rorschach-scoring/static/js/2.7ee57c45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cde4594f9a279d87d9e8",
+    "url": "/rorschach-scoring/static/js/main.d513d4d0.chunk.js"
+  },
+  {
+    "revision": "173f03f882e9de016b09",
+    "url": "/rorschach-scoring/static/js/runtime-main.0fb046a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rorschach-scoring/static/media/logo.5d5d9eef.svg"
+  }
+]);
